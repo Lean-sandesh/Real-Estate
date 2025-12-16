f129feeebb49a1f37ec988d152bd5a6f5d778beb
Lean-sandesh <sandeshzele09@gmail.com>
2025-12-16 15:28:11 +0530

    updated register

--- real-estate-frontend/src/services/userService.js
+++ real-estate-frontend/src/services/userService.js
@@ -1,7 +1,7 @@
 import api from "./api"; // your axios instance
 
 const updateProfile = async (formData) => {
-  return await api.put("/auth/update-profile", formData, {
+  return await api.put("/auth/profile", formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 };
